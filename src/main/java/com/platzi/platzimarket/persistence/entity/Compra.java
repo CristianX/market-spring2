dc183457521d673da279db7e7176c6c4ab05e418
CristianX <thecristianx@hotmail.com>
2022-11-14 20:49:06 -0500

    Añadiendo mapeo de tablas de Producto y Compra

--- src/main/java/com/platzi/platzimarket/persistence/entity/Compra.java
+++ src/main/java/com/platzi/platzimarket/persistence/entity/Compra.java
@@ -28,7 +28,56 @@ public class Compra {
     private String medioPago;
 
     private String comentario;
-
     private String estado;
+
+
+
+    public Integer getIdCompra() {
+        return this.idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getIdCliente() {
+        return this.idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return this.fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedioPago() {
+        return this.medioPago;
+    }
+
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return this.comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 
 }
