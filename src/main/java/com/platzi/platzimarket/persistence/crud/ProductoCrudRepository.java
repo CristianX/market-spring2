8e018b76c9a457beb5b73c3b85f71e2251f00cab
CristianX <thecristianx@hotmail.com>
2022-11-14 22:52:43 -0500

    Implementación inicial de Query methods

--- src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -1,9 +1,20 @@
 package com.platzi.platzimarket.persistence.crud;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.platzi.platzimarket.persistence.entity.Producto;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
-    
+
+    // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery
+    // = true)
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    // LessThanAnd para implementar la operación de menor que y verificar si el
+    // Estado es true
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
